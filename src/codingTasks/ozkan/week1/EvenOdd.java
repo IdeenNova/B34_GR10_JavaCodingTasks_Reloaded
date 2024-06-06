57997d32fc8e5547352f691f0a92925cec21d068
IdeenNova <ideen@cydeo.com>
2024-06-06 11:33:52 -0400

    Initial commit

---
+++ src/codingTasks/ozkan/week1/EvenOdd.java
@@ -0,0 +1,13 @@
+package codingTasks.ozkan.week1;
+
+
+public class EvenOdd {
+    /*
+Write a method which can identifies given number is even or odd.
+Ex:
+idenitify(5) -> "Odd"
+idenitify(6) -> "Even"
+*/
+
+
+}
---
+++ src/codingTasks/tahmina/week1/EvenOdd.java
@@ -0,0 +1,20 @@
+package codingTasks.tahmina.week1;
+
+
+public class EvenOdd {
+    /*
+Write a method which can identifies given number is even or odd.
+Ex:
+idenitify(5) -> "Odd"
+idenitify(6) -> "Even"
+*/
+    public static void oddEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is even number");
+        } else {
+            System.out.println(num + " is odd number");
+        }
+
+
+    }
+}
