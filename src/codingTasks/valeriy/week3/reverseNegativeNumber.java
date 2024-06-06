915bef78c45a6aff0b43bda7a3dfc9593a11b20d
Valeriy Syutkin <valeriysyutkin86@gmail.com>
2024-06-06 15:28:33 -0400

    set up 3rd week 2nd task requirements

---
+++ src/codingTasks/valeriy/week3/reverseNegativeNumber.java
@@ -0,0 +1,8 @@
+package codingTasks.valeriy.week3;
+
+//Write a return method that can reverse negative number and return it as int
+//ex: given int is ==> -53
+//  the output will be ==> -35
+
+public class reverseNegativeNumber {
+}
