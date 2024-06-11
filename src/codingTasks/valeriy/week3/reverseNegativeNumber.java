7a9ce1ff740032beb8446a8362e0652f06afe08a
Valeriy Syutkin <valeriysyutkin86@gmail.com>
2024-06-11 03:03:42 -0400

    reverseNegativeNumber task is done

--- src/codingTasks/valeriy/week3/reverseNegativeNumber.java
+++ src/codingTasks/valeriy/week3/reverseNegativeNumber.java
@@ -5,4 +5,38 @@ package codingTasks.valeriy.week3;
 //  the output will be ==> -35
 
 public class reverseNegativeNumber {
+
+    public static void main(String[] args) {
+        int number = -53;
+        int reversedNumber = revereNumber(number);
+        System.out.println("Reversed number: " + reversedNumber);
+    }
+
+
+
+
+
+    public static int revereNumber(int number){
+
+        String str = Integer.toString(number);
+        String reversedStr = "";
+
+
+        if (str.charAt(0) == '-') {
+            reversedStr += '-';
+
+            for (int i = str.length() - 1; i > 0; i--) {
+                reversedStr += str.charAt(i);
+            }
+        } else {
+
+            for (int i = str.length() - 1; i >= 0; i--) {
+                reversedStr += str.charAt(i);
+            }
+        }
+
+        return Integer.parseInt(reversedStr);
+    }
+
+
 }
