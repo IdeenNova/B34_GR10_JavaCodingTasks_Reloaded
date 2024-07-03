7b4ef8abf6aa37d0caf0cf1f845887d795fde931
Valeriy Syutkin <valeriysyutkin86@gmail.com>
2024-07-03 00:31:37 -0400

    "sumOfDigits" task is done+bonus task

---
+++ src/codingTasks/valeriy/week6/sumOfDigits.java
@@ -0,0 +1,23 @@
+package codingTasks.valeriy.week6;
+
+public class sumOfDigits {
+
+ //Write a method that can return the sum of the digits in a string
+
+    public static void main(String[] args) {
+        System.out.println(sumOfDigitsInString("qwer1235qwe"));
+    }
+
+
+
+    public static int sumOfDigitsInString(String stringWithDigits) {
+        int sum = 0;
+        for (char eachChar : stringWithDigits.toCharArray()) {
+            if (Character.isDigit(eachChar)) {
+                sum += Character.getNumericValue(eachChar);
+            }
+        }
+        return sum;
+    }
+
+}
