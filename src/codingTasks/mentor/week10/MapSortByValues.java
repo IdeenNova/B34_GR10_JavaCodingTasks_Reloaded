263c318fd657228f713ed1e328e329d866d276ff
IdeenNova <ideen@cydeo.com>
2024-07-30 13:32:52 -0400

    add new tasks

---
+++ src/codingTasks/mentor/week10/MapSortByValues.java
@@ -0,0 +1,13 @@
+package codingTasks.mentor.week10;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapSortByValues {
+
+    //Write a method that can sort the Map by values
+
+
+}
