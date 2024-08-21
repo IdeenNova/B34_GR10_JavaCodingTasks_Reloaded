4a27d90ea48514ac07391fc901ae7b774a4bca0b
viktoriiahorpyniuk <vhorpyniuk@gmail.com>
2024-08-21 00:01:51 -0400

    week 11 done

---
+++ src/codingTasks/viktoriia/week10/ArrayListSortAscending.java
@@ -0,0 +1,10 @@
+package codingTasks.viktoriia.week10;
+
+public class ArrayListSortAscending {
+/*
+Write a method that can sort the ArrayList in Ascending order without using
+the sort method.
+ */
+
+}
+
---
+++ src/codingTasks/viktoriia/week10/ArrayListSortDescending.java
@@ -0,0 +1,6 @@
+package codingTasks.viktoriia.week10;
+
+public class ArrayListSortDescending {
+    //Write a method that can sort the ArrayList in descending order without using the sort method
+
+}
