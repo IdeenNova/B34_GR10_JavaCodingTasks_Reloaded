0ce3235a0cb20a696d9e4091cf91a5138fecfb85
viktoriiahorpyniuk <vhorpyniuk@gmail.com>
2024-08-20 23:49:52 -0400

    week 11 done

---
+++ src/codingTasks/viktoriia/week11/MapFrequencyOFCharacters.java
@@ -0,0 +1,37 @@
+package codingTasks.viktoriia.week11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapFrequencyOFCharacters {
+  //  Write a method that prints the frequency of each character from a String.
+  public static void printCharacterFrequency(String input) {
+      // Create a HashMap to store the frequency of each character
+      Map<Character, Integer> frequencyMap = new HashMap<>();
+
+      // Convert the input string to a character array
+      char[] characters = input.toCharArray();
+
+      // Iterate over the character array
+      for (char c : characters) {
+          // If the character is already in the map, increment its count
+          if (frequencyMap.containsKey(c)) {
+              frequencyMap.put(c, frequencyMap.get(c) + 1);
+          } else {
+              // Otherwise, add the character to the map with a count of 1
+              frequencyMap.put(c, 1);
+          }
+      }
+
+      // Print the frequency of each character
+      for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+          System.out.println("Character '" + entry.getKey() + "' appears " + entry.getValue() + " time(s).");
+      }
+  }
+
+    public static void main(String[] args) {
+        String input = "example";
+        printCharacterFrequency(input);
+    }
+
+}
---
+++ src/codingTasks/viktoriia/week11/MapMinValue.java
@@ -0,0 +1,34 @@
+package codingTasks.viktoriia.week11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMinValue {
+    //Write a method that can return the minimum value from a map (DO NOT use sort method)
+
+    public static int findMinValue(Map<String, Integer> map) {
+        // Initialize a variable to store the minimum value, starting with the maximum possible integer value
+        int minValue = Integer.MAX_VALUE;
+
+        // Iterate over the values in the map
+        for (int value : map.values()) {
+            // If the current value is less than minValue, update minValue
+            if (value < minValue) {
+                minValue = value;
+            }
+        }
+
+        return minValue;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> exampleMap = new HashMap<>();
+        exampleMap.put("A", 10);
+        exampleMap.put("B", 3);
+        exampleMap.put("C", 8);
+        exampleMap.put("D", 15);
+
+        int minValue = findMinValue(exampleMap);
+        System.out.println("The minimum value in the map is: " + minValue);
+    }
+}
