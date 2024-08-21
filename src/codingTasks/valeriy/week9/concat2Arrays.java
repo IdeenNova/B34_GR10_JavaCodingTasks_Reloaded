ad94e4b16f517ecb773800cfc76a9e6bef2ce7e6
Valeriy Syutkin <valeriysyutkin86@gmail.com>
2024-08-21 02:16:02 -0400

    Week9 tasks are completed

--- src/codingTasks/valeriy/week9/concat2Arrays.java
+++ src/codingTasks/valeriy/week9/concat2Arrays.java
@@ -1,9 +1,37 @@
 package codingTasks.valeriy.week9;
 
+import java.util.Arrays;
+
 public class concat2Arrays {
 
 //Write a return method that can concate two arrays
 
+    public static int[] concatenate(int[] array1, int[] array2) {
+        int[] result = new int[array1.length + array2.length];
 
+
+        int index = 0;
+        for (int i = 0; i < array1.length; i++) {
+            result[index++] = array1[i];
+        }
+
+
+        for (int i = 0; i < array2.length; i++) {
+            result[index++] = array2[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {2, 3, 5, 7};
+        int[] arr2 = {6, 9, 11,14};
+
+        int[] concatenatedArray = concatenate(arr1, arr2);
+
+
+        System.out.println((Arrays.toString(concatenatedArray)));
+
+    }
 
 }
